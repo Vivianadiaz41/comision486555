75a0ca05627c44bf5954b219e2945384ab4c301c
viviana <vivianadiazweb@gmail.com>
2023-02-09 20:09:36 -0300

    entrega2

--- main1.js
+++ main1.js
@@ -1,30 +1,45 @@
 //clases
 class Producto {
-    constructor(id, nombre, precio, cantidad, descripcion) {
-      this.id = id;
-      this.nombre = nombre;
-      this.precio = Number(precio);
-      this.cantidad = Number(cantidad);
-      this.descripcion = descripcion;
-      this.vendido = false;
-      this.subtotal = 0;
-    }
-    //metodo
-    sumaIva() {
-      this.precio = this.precio * 1.21;
-    }
+  constructor(id, nombre, precio, cantidad, descripcion) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = Number(precio);
+    this.cantidad = Number(cantidad);
+    this.descripcion = descripcion;
+    this.vendido = false;
+    this.subtotal = 0;
   }
-  //declaracionarray
-  const productos = [];
-  productos.push(new Producto(1, "arroz", "50", "2", "Es un buen producto"));
-  productos.push(new Producto(2, "poroto", "20", "3", "Es un buen producto"));
-  productos.push(new Producto(3, "fideo", "30", "1", "Es un buen producto"));
-  console.log(productos);
-  //funcion
+  //metodo
+  sumaIva() {
+    this.precio = this.precio * 1.21;
+  }
+}
+//declaracionarray
+const productos = [];
+productos.push(
+  new Producto(
+    1,
+    "Anteojos luna",
+    "5000",
+    "20",
+    "Disponibles marcos rojos  y trasparentes"
+  )
+);
+productos.push(
+  new Producto(2, "Anteojos Monroe", "20000", "10", "Disponibles venta On line")
+);
+productos.push(
+  new Producto(3, "Anteojos Reef", "12400", "11", "Stock limitado")
+);
+console.log(productos);
+//funcion
+function subtotal() {
   for (const Producto of productos) {
     Producto.sumaIva();
     Producto.subtotal = Producto.precio * Producto.cantidad;
     Producto.vendido = true;
   }
-  subtotal();
-  
+}
+
+subtotal();
+console.log(subtotal);
